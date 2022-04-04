16f656bc853a985a74d7be76d57c49358e2e7c7f
raveena69 <raveena1996@gmail.com>
2022-04-04 23:56:49 +0530

    Update the file structure

---
+++ lib/quizPages/question.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import 'package:guess_the_word/database/quizDb.dart';
+import 'package:guess_the_word/database/questionItem.dart';
+import 'package:guess_the_word/database/quizItem.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ViewQuestions extends StatefulWidget{
+  final QuizItem quiz;
+
+  ViewQuestions({this.quiz});
+
+  @override
+  State<StatefulWidget> createState() => _ViewQuestionsState(quiz: this.quiz);
+}
+
+class _ViewQuestionsState extends State<ViewQuestions>{
+
+  final QuizItem quiz;
+
+  int index = 0;
+  int score = 0;
+
+  _ViewQuestionsState({this.quiz});
+
+  List<QuestionItem> questions  = [];
+  List<Widget> get questionsList => questions.map((item) => formatQuestions(item)).toList();
+
+  void initState(){
+    super.initState();
+    fetchQuestions();
+  }
+
+  void dispose() {
+    super.dispose();
+  }
+
+  void fetchQuestions() async{
+    List<Map<String, dynamic>> _results = await DB.queryQuestions(QuestionItem.table, quiz);
+    questions = _results.map((quiz) => QuestionItem.fromMap(quiz)).toList();
+    setState(() {});
+  }
+
+  Widget formatQuestions(QuestionItem question){
+    return Container(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(question.question,
+            style:
+            GoogleFonts.montserrat(color: Colors.black, fontSize: 20),),
+          SizedBox(height: 50),
+          Column(children:answerButtons(question)),
+          SizedBox(height: 150),
+        ],
+      )
+    );
+  }
+
+  Widget answerButton(String s, bool isCorrect){
+    void action(){
+      if(isCorrect){
+        setState(() {
+          index += 1;
+          score += 1;
+        });
+      }
+      else{
+        setState(() {
+          index += 1;
+        });
+      } 
+    }
+
+    return Container(
+      margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
+      width: double.infinity,
+      height: 60,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(6),
+        gradient: LinearGradient(colors: [Colors.blue[700], Colors.blue[400]]),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 5,
+            offset: Offset(0, 5)
+          )
+        ]
+      ),
+      child:
+      FlatButton(child:
+      Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(s, style: GoogleFonts.montserrat(
+                    color: Colors.white,
+                    fontSize: 16
+                  ),
+                ),  
+              ],
+            ),
+          ]
+        )
+      ), 
+      onPressed: () => action()),
+    );
+  }
+
+  void correct(){
+    setState(() {
+      index += 1;
+      score += 1;
+    });
+  }
+
+  void incorrect(){
+    setState(() {
+      index += 1;
+    });
+  }
+
+  List<Widget> answerButtons(QuestionItem q){
+    List<Widget> buttons = [];
+    buttons.add(answerButton(q.correctAnswer, true));
+    buttons.add(answerButton(q.wrongAnswerA, false));
+    buttons.add(answerButton(q.wrongAnswerB, false));
+    buttons.add(answerButton(q.wrongAnswerC, false));
+    buttons.shuffle();
+    return buttons;
+  }
+
+  Widget body(){
+    if(questionsList.length == index){
+      return Container(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("You got a score of  " + score.toString() + " / " + index.toString() + " !", style: Theme.of(context).primaryTextTheme.headline1),
+            SizedBox(height: 200)
+          ],
+        )
+      );
+    } else{
+      return questionsList[index];
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+        title: Text(
+            quiz.name,
+          style: GoogleFonts.montserrat(color: Colors.white, fontSize: 16),
+        ),
+      ),
+      body: body()
+    );
+  }
+}
---
+++ lib/specialNote/form/special_notes_form_state.dart
@@ -0,0 +1,36 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:guess_the_word/database/specialNotesModal.dart';
+
+abstract class NotesFormState extends Equatable {
+
+  final String message;
+  final Notes note;
+  const NotesFormState({this.message, this.note});
+
+  @override
+  List<Object> get props => [];
+}
+
+class InitialNotesFormState extends NotesFormState {}
+
+class NotesFormLoading extends NotesFormState {}
+
+class NotesFormError extends NotesFormState {
+  final String errorMessage;
+
+  const NotesFormError(this.errorMessage);
+
+  List<Object> get props => [errorMessage];
+
+  @override
+  String toString() => 'Notes Form No Data (message : $errorMessage)';
+}
+
+class NoteHasData extends NotesFormState {
+  const NoteHasData({@required Notes note}) : super(note : note);
+}
+
+class Success extends NotesFormState {
+  Success({@required String successMessage}) : super(message: successMessage);
+}
